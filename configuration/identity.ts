e9d03c73e5af49b457fdb1c0acfc3aca17a8bc78
aaronsingletary0 <aaronsingletary0@gmail.com>
2025-03-03 23:09:46 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[Aaron Singletary]`;
-export const OWNER_DESCRIPTION: string = `[UNC Student]`;
+export const OWNER_NAME: string = `Aaron Singletary`;
+export const OWNER_DESCRIPTION: string = `UNC Student`;
 
 export const AI_NAME: string = `ACE`;
 export const AI_TONE: string = `[AI TONE]`;
