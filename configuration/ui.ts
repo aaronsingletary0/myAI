7587d61194a81a75254b6c2b128c7b8c0f3886a5
aaronsingletary0 <aaronsingletary0@gmail.com>
2025-03-04 15:30:36 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `AI Interview and Resume Coach`;
 export const FOOTER_MESSAGE: string = `Customized AI by Aaron Singletary @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
 export const PAGE_TITLE: string = `ACE`;
-export const PAGE_DESCRIPTION: string = `Chat with ${ACE}, your ${AI Interview and Resume Coach}.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, your ${OWNER_NAME}.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
