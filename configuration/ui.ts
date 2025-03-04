392987d6d448f6ffa8d1d5a299407b5d6790d094
aaronsingletary0 <aaronsingletary0@gmail.com>
2025-03-03 22:58:25 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[ACE]`;
-export const MESSAGE_PLACEHOLDER: string = `[AI Interview and Resume Coach]`;
-export const FOOTER_MESSAGE: string = `[Customized AI by Aaron Singletary @ UNC]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR]`;
-export const PAGE_TITLE: string = `[ACE]`;
+export const CHAT_HEADER: string = `ACE`;
+export const MESSAGE_PLACEHOLDER: string = `AI Interview and Resume Coach`;
+export const FOOTER_MESSAGE: string = `Customized AI by Aaron Singletary @ UNC`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR`;
+export const PAGE_TITLE: string = `ACE`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
