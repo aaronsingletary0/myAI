bfc9e4c5cb418bed9df6eefc445ba33b66998d1f
aaronsingletary0 <aaronsingletary0@gmail.com>
2025-03-15 16:23:52 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -2,6 +2,6 @@ import { OWNER_NAME, AI_NAME } from "./identity";
 
 export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `I'm thinking... but I might need a little more info. Try rephrasing or asking differently!`;
-export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
+export const WORD_CUTOFF: number = 7000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
-export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+export const HISTORY_CONTEXT_LENGTH: number = 6; // Number of messages to use for context when generating a response
